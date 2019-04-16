9f9357118777b7b7a7f33aae8cf0932db881b80a
20080904@163.com <yangyz@YANGYZ-CN.cn.oracle.com>
2019-04-16 17:40:34 +0800

    js prototype

---
+++ javascript+nodejs/JSPrototype/objectandfunction.js
@@ -0,0 +1,25 @@
+/*JavaScript 中，万物皆对象！但对象也是有区别的。
+分为普通对象和函数对象，Object 、Function 是 JS 自带的函数对象。下面举例说明*/
+var o1={};
+var o2=new Object();
+var o3=new f1();
+
+function f1(){};
+var f2=function(){};
+var f3=new Function('str','console.log(str)');
+
+console.log(typeof Object);
+console.log(typeof Function);
+console.log('-------------------');
+console.log(typeof f1);
+console.log(typeof f2);
+console.log(typeof f3);
+console.log('-------------------');
+console.log(typeof o1);
+console.log(typeof o2);
+console.log(typeof o3);
+/*在上面的例子中 o1 o2 o3 为普通对象，f1 f2 f3 为函数对象。
+怎么区分，其实很简单，凡是通过 new Function() 创建的对象都是函数对象，
+其他的都是普通对象。f1,f2,归根结底都是通过 new Function()的方式进行创建的。
+Function Object 也都是通过 New Function()创建的。
+*/
